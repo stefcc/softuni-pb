f14653a020a545909c2686d7654eb32b0bb3c9a7
stefcc <st.tsanev@gmail.com>
2023-03-11 20:38:54 +0200

    Add some scripts to the repo

---
+++ math.js
@@ -0,0 +1,10 @@
+function solve(input){
+    
+    // Compilers calculate like that
+
+    let a = Number(input[0]);
+    let b = Number(input[1]);
+    console.log(a + b);
+}
+
+solve(["0.1", "0.2"]);
