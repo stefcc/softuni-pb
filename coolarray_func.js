027d79ab926e6400156c23f9c93cd59f74d86d09
stefcc <st.tsanev@gmail.com>
2023-03-14 23:33:59 +0200

    Add new js file

--- coolarray_func.js
+++ coolarray_func.js
@@ -1,8 +1,8 @@
 const grades = [80, 88, 94, 98, 100];
-const isHighPerforminClass = grades.every(grade =>{
+const isHighPerformingClass = grades.every(grade =>{
     return grade >= 80;
 }); 
 
-console.log(isHighPerforminClass);
+console.log(isHighPerformingClass);
 
 
